c6c93193f16bb49251bfeba9e718baec54b95f25
dieperdev <140034404+dieperdev@users.noreply.github.com>
2026-05-06 19:22:59 -0400

    gofmt and more test cases
    added case-sensitive + case-insensitive test cases for `HighlightColor` and `HighlightSameReplacements`

--- highlight_test.go
+++ highlight_test.go
@@ -1,11 +1,12 @@
 package highlight
 
 import (
-"fmt"
+	"fmt"
+	"strings"
 	"testing"
 )
 
-type testCase struct {
+type highlightTestCase struct {
 	original      string
 	substring     string
 	limit         int
@@ -17,9 +18,31 @@ type testCase struct {
 	want     string
 }
 
-var testCases = []testCase{
+type highlightColorTestCase struct {
+	original      string
+	substring     string
+	limit         int
+	color         string
+	caseSensitive bool
+
+	testName string
+	want     string
+}
+
+type highlightSameReplacementsTestCase struct {
+	original      string
+	substring     string
+	limit         int
+	replacement   string
+	caseSensitive bool
+
+	testName string
+	want     string
+}
+
+var highlightTestCases = []highlightTestCase{
 	// Case-insensitive test cases
-	testCase{
+	highlightTestCase{
 		original:      "hello world",
 		substring:     "world",
 		limit:         1,
@@ -30,7 +53,7 @@ var testCases = []testCase{
 		testName: "\"hello world\", limit: 1, case-sensitive: false",
 		want:     "hello testing world.",
 	},
-	testCase{
+	highlightTestCase{
 		original:      "hello world",
 		substring:     "world",
 		limit:         2,
@@ -41,7 +64,7 @@ var testCases = []testCase{
 		testName: "\"hello world\", limit: 2, case-sensitive: false",
 		want:     "hello testing world.",
 	},
-	testCase{
+	highlightTestCase{
 		original:      "hello world world world",
 		substring:     "world",
 		limit:         2,
@@ -52,7 +75,7 @@ var testCases = []testCase{
 		testName: "\"hello world world world\", limit: 2, case-sensitive: false",
 		want:     "hello testing world. testing world. world",
 	},
-	testCase{
+	highlightTestCase{
 		original:      "hello world world world",
 		substring:     "world",
 		limit:         3,
@@ -65,7 +88,7 @@ var testCases = []testCase{
 	},
 
 	// Case-sensitive test cases
-	testCase{
+	highlightTestCase{
 		original:      "hello world",
 		substring:     "World",
 		limit:         1,
@@ -76,7 +99,7 @@ var testCases = []testCase{
 		testName: "\"hello world\", limit: 1, case-sensitive: true",
 		want:     "hello world",
 	},
-	testCase{
+	highlightTestCase{
 		original:      "hello World",
 		substring:     "World",
 		limit:         2,
@@ -87,7 +110,7 @@ var testCases = []testCase{
 		testName: "\"hello World\", limit: 2, case-sensitive: true",
 		want:     "hello testing World.",
 	},
-	testCase{
+	highlightTestCase{
 		original:      "hello WORLD WORLD WORLD",
 		substring:     "WORLD",
 		limit:         2,
@@ -98,7 +121,7 @@ var testCases = []testCase{
 		testName: "\"hello WORLD WORLD WORLD\", limit: 2, case-sensitive: true",
 		want:     "hello testing WORLD. testing WORLD. WORLD",
 	},
-	testCase{
+	highlightTestCase{
 		original:      "hello woRld woRld woRld",
 		substring:     "world",
 		limit:         3,
@@ -111,12 +134,146 @@ var testCases = []testCase{
 	},
 }
 
+var highlightColorTestCases = []highlightColorTestCase{
+	// Case-insensitive test cases
+	highlightColorTestCase{
+		original:      "hello world",
+		substring:     "world",
+		limit:         1,
+		color:         FgBlack,
+		caseSensitive: false,
+
+		testName: "\"hello world\", limit: 1, case-sensitive: false",
+		want:     fmt.Sprintf("hello %sworld%s", FgBlack, FgBlack),
+	},
+	highlightColorTestCase{
+		original:      "hello world",
+		substring:     "world",
+		limit:         2,
+		color:         FgBlack,
+		caseSensitive: false,
+
+		testName: "\"hello world\", limit: 2, case-sensitive: false",
+		want:     fmt.Sprintf("hello %sworld%s", FgBlack, FgBlack),
+	},
+	highlightColorTestCase{
+		original:      "hello world world world",
+		substring:     "world",
+		limit:         2,
+		color:         FgBlack,
+		caseSensitive: false,
+
+		testName: "\"hello world world world\", limit: 2, case-sensitive: false",
+		want:     fmt.Sprintf("hello %sworld%s %sworld%s world", FgBlack, FgBlack, FgBlack, FgBlack),
+	},
+	highlightColorTestCase{
+		original:      "hello world world world",
+		substring:     "world",
+		limit:         3,
+		color:         FgBlack,
+		caseSensitive: false,
+
+		testName: "\"hello world world world\", limit: 3, case-sensitive: false",
+		want:     fmt.Sprintf("hello %sworld%s %sworld%s %sworld%s", FgBlack, FgBlack, FgBlack, FgBlack, FgBlack, FgBlack),
+	},
+
+	// Case-sensitive test cases
+	highlightColorTestCase{
+		original:      "hello world",
+		substring:     "World",
+		limit:         1,
+		color:         FgBlack,
+		caseSensitive: true,
+
+		testName: "\"hello world\", limit: 1, case-sensitive: true",
+		want:     "hello world",
+	},
+	highlightColorTestCase{
+		original:      "hello World",
+		substring:     "World",
+		limit:         2,
+		color:         FgBlack,
+		caseSensitive: true,
+
+		testName: "\"hello World\", limit: 2, case-sensitive: true",
+		want:     fmt.Sprintf("hello %sWorld%s", FgBlack, FgBlack),
+	},
+	highlightColorTestCase{
+		original:      "hello WORLD WORLD WORLD",
+		substring:     "WORLD",
+		limit:         2,
+		color:         FgBlack,
+		caseSensitive: true,
+
+		testName: "\"hello WORLD WORLD WORLD\", limit: 2, case-sensitive: true",
+		want:     fmt.Sprintf("hello %sWORLD%s %sWORLD%s WORLD", FgBlack, FgBlack, FgBlack, FgBlack),
+	},
+	highlightColorTestCase{
+		original:      "hello woRld woRld woRld",
+		substring:     "world",
+		limit:         3,
+		color:         FgBlack,
+		caseSensitive: true,
+
+		testName: "\"hello woRld woRld woRld\", limit: 3, case-sensitive: true",
+		want:     "hello woRld woRld woRld",
+	},
+}
+
+var highlightSameReplacementsTestCases = make([]highlightSameReplacementsTestCase, len(highlightColorTestCases))
+
+// Fill the `SameReplacements` test cases with the same data as the `Color`
+// test cases but change the field name to `replacement` and change the
+// replacement string to "new".
+func init() {
+	for i, tCase := range highlightColorTestCases {
+		highlightSameReplacementsTestCases[i] = highlightSameReplacementsTestCase{
+			original:      tCase.original,
+			substring:     tCase.substring,
+			limit:         tCase.limit,
+			replacement:   strings.ReplaceAll(tCase.color, FgBlack, "new"),
+			caseSensitive: tCase.caseSensitive,
+
+			testName: tCase.testName,
+			want:     strings.ReplaceAll(tCase.want, FgBlack, "new"),
+		}
+	}
+}
+
 func TestHighlight(t *testing.T) {
-	for _, tCase := range testCases {
+	for _, tCase := range highlightTestCases {
 		testName := fmt.Sprintf("testing %s", tCase.testName)
 
 		t.Run(testName, func(t *testing.T) {
 			highlighted := Highlight(tCase.original, tCase.substring, tCase.limit, tCase.before, tCase.after, tCase.caseSensitive)
+
+			if highlighted != tCase.want {
+				t.Errorf("got %q, want %q", highlighted, tCase.want)
+			}
+		})
+	}
+}
+
+func TestHighlightColor(t *testing.T) {
+	for _, tCase := range highlightColorTestCases {
+		testName := fmt.Sprintf("testing %s", tCase.testName)
+
+		t.Run(testName, func(t *testing.T) {
+			highlighted := HighlightColor(tCase.original, tCase.substring, tCase.limit, tCase.color, tCase.caseSensitive)
+
+			if highlighted != tCase.want {
+				t.Errorf("got %q, want %q", highlighted, tCase.want)
+			}
+		})
+	}
+}
+
+func TestHighlightSameReplacements(t *testing.T) {
+	for _, tCase := range highlightSameReplacementsTestCases {
+		testName := fmt.Sprintf("testing %s", tCase.testName)
+
+		t.Run(testName, func(t *testing.T) {
+			highlighted := HighlightSameReplacements(tCase.original, tCase.substring, tCase.limit, tCase.replacement, tCase.caseSensitive)
 
 			if highlighted != tCase.want {
 				t.Errorf("got %q, want %q", highlighted, tCase.want)
