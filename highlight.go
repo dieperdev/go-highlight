44738391b1d06d58de7b0c8297757a38f09b5fad
dieperdev <140034404+dieperdev@users.noreply.github.com>
2026-05-06 18:07:04 -0400

    add documentation to functions and add `HighlightSameReplacements` function

--- highlight.go
+++ highlight.go
@@ -4,6 +4,11 @@ import (
 	gostring "github.com/boyter/go-string"
 )
 
+// Highlight prefixes up to `limit` occurrences of `substring`
+// with `before` and suffixes up to `limit` occurrences of
+// `substring` with `after`.
+//
+// Highlight can optionally be case-sensitive.
 func Highlight(original string, substring string, limit int, before string, after string, caseSensitive bool) string {
 	var positions [][]int
 
@@ -18,6 +23,19 @@ func Highlight(original string, substring string, limit int, before string, afte
 	return highlighted
 }
 
+// HighlightColor has the same functionality as Highlight, but prefixes and
+// suffixes up to `limit` occurences of `substring` with the same text.
+//
+// The `color` passed to HighlightColor does not have to strictly be a color.
+// You may want to use `HighlightSameReplacements` to make usage of
+// `go-highlight` more clear in your program if `color` is not a color.
 func HighlightColor(original string, substring string, limit int, color string, caseSensitive bool) string {
 	return Highlight(original, substring, limit, color, color, caseSensitive)
+}
+
+// HighlightSameReplacements has the same functionality as HighlightColor, but
+// with different function name. It is intended to make usage of `go-highlight`
+// more clear.
+func HighlightSameReplacements(original string, substring string, limit int, replacement string, caseSensitive bool) string {
+	return Highlight(original, substring, limit, replacement, replacement, caseSensitive)
 }
